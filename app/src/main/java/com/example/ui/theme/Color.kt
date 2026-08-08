ebfc42b4361e0fb7c65077f8af41cb0d6cb15d0f
Arafat <arafatmans549@gmail.com>
2026-08-08 12:54:07 -0700

    Geese

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Scheme
+val PrimaryLight = Color(0xFF1E64DC)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD8E2FF)
+val OnPrimaryContainerLight = Color(0xFF001A41)
+val SecondaryLight = Color(0xFF535F70)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFD7E3F8)
+val OnSecondaryContainerLight = Color(0xFF101C2B)
+val SurfaceLight = Color(0xFFF9F9FF)
+val OnSurfaceLight = Color(0xFF191C20)
+val SurfaceContainerLight = Color(0xFFEEF0F8)
+
+// Dark Scheme
+val PrimaryDark = Color(0xFFADC6FF)
+val OnPrimaryDark = Color(0xFF002E69)
+val PrimaryContainerDark = Color(0xFF004494)
+val OnPrimaryContainerDark = Color(0xFFD8E2FF)
+val SecondaryDark = Color(0xFFBBC7DB)
+val OnSecondaryDark = Color(0xFF253140)
+val SecondaryContainerDark = Color(0xFF3B4758)
+val OnSecondaryContainerDark = Color(0xFFD7E3F8)
+val SurfaceDark = Color(0xFF111318)
+val OnSurfaceDark = Color(0xFFE2E2E9)
+val SurfaceContainerDark = Color(0xFF1D2026)
+
